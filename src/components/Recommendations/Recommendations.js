7285c9c66c0e9fd7fba1137eb9e405f8eb4aa878
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-28 17:42:18 -0500

    add placeholder elements to recommendations

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -11,21 +11,53 @@ function Recommendations(props) {
   return (
     <Grid container spacing={3}>
       <Grid item xs={12}>
-      <Typography className={classes.padding} align="center" variant="h5">
-        You might also like
-      </Typography>
+        <Typography className={classes.padding} align="center" variant="h5">
+          You might also like
+        </Typography>
       </Grid>
       <Grid item xs={6} md={4} lg={3}>
-        <Product/>
+        <Product
+          name={"placeholder ".repeat(70)}
+          price={1.00}
+          description={"some random crap"}
+          listPrice={0}
+          onSale={false}
+          new={false}
+          img={"https://placehold.it/690x400"}
+        />
       </Grid>
       <Grid item xs={6} md={4} lg={3}>
-        <Product/>
+        <Product
+          name={"placeholder ".repeat(70)}
+          price={1.00}
+          description={"some random crap"}
+          listPrice={0}
+          onSale={false}
+          new={false}
+          img={"https://placehold.it/690x400"}
+        />
       </Grid>
       <Grid item xs={6} md={4} lg={3}>
-        <Product/>
+        <Product
+          name={"placeholder ".repeat(70)}
+          price={1.00}
+          description={"some random crap"}
+          listPrice={0}
+          onSale={false}
+          new={false}
+          img={"https://placehold.it/690x400"}
+        />
       </Grid>
       <Grid item xs={6} md={4} lg={3}>
-        <Product/>
+        <Product
+        name={"placeholder ".repeat(70)}
+        price={1.00}
+        description={"some random crap"}
+        listPrice={0}
+        onSale={false}
+        new={false}
+        img={"https://placehold.it/690x400"}
+        />
       </Grid>
     </Grid>
   );
