016ac43eff9c15acdc50ce8886c0f33dfc158fd1
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-28 17:40:46 -0500

    render product detail from api

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,56 +1,79 @@
 import React from 'react';
 
+import axios from 'axios';
+
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-
+import { withStyles } from '@material-ui/core/styles';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-
 import Rating from '@material-ui/lab/Rating';
-
-import Recommendations from '../Recommendations/Recommendations';
 
 import styles from './ProductDetail.styles';
 
-function ProductDetail(props) {
-  const classes = styles();
+class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      product: {}
+    };
+  }
 
-  return(
-    <>
-    <div className={classes.detailPadding}>
-      <Grid container spacing={3}>
-        <Grid item lg={6} xs={12}>
-          <img style={{width: '100%'}} src="https://placehold.it/1920x1080"/>
+  getItem() {
+    axios.get(`/v1/products/${this.props.slug}`)
+    .then(res => this.setState({
+      loading: false,
+      product: res.data
+    }))
+    .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getItem();
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    if (this.state.loading) {
+      return(<p>Loading...</p>);
+    }
+
+    return(
+      <div className={classes.detailPadding}>
+        <Grid container spacing={3}>
+          <Grid item lg={6} xs={12}>
+            <img style={{width: '100%'}} src={this.state.product.images.img1920x1080}/>
+          </Grid>
+          <Grid item lg={6} xs={12}>
+            <Typography variant="h6" gutterBottom={true}>
+              {this.state.product.name}
+            </Typography>
+            <Rating className={classes.rating} name="size-small" value={4} readOnly={true} size="small" />
+            <Box className={classes.price} display="flex">
+              <Typography variant="h5">
+                ${this.state.product.price.toFixed(2)}
+              </Typography>
+              {this.state.product.on_sale ?
+              <Typography variant="h5" className={classes.listPrice}>
+                <span className={classes.strikeOutText}>${this.state.product.list_price.toFixed(2)}</span>
+              </Typography> : null}
+            </Box>
+            <Typography className={classes.description} variant="body2">
+              {this.state.product.description}
+            </Typography>
+            <Button className={classes.buttonMargin} variant="contained" size="small" color="primary">
+              <Typography variant="button">
+                Add to Cart <ShoppingCartIcon className={classes.cartIcon}/>
+              </Typography>
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item lg={6} xs={12}>
-          <Typography variant="h6" gutterBottom={true}>
-            iRobot Roomba 670 Robot Vacuum-Wi-Fi Connectivity,
-          </Typography>
-          <Rating className={classes.rating} name="size-small" value={4} readOnly={true} size="small" />
-          <Box className={classes.price} display="flex">
-            <Typography variant="h5">
-              $86.00
-            </Typography>
-            <Typography variant="h5" className={classes.listPrice}>
-              <span className={classes.strikeOutText}>$192.00</span>
-            </Typography>
-          </Box>
-          <Typography className={classes.description} variant="body2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit libero eos atque quia ipsa sint voluptatibus! Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.
-          </Typography>
-          <Button variant="contained" size="small" color="primary">
-            <Typography variant="button">
-              Add to Cart <ShoppingCartIcon className={classes.cartIcon}/>
-            </Typography>
-          </Button>
-        </Grid>
-      </Grid>
-    </div>
-    <hr/>
-    <Recommendations/>
-    </>
-  );
+      </div>
+    );
+  }
 }
 
-export default ProductDetail;
+export default withStyles(styles)(ProductDetail);
