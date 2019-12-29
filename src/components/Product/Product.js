9dad0b5e5cff524250106b44f30b6cadddfa175b
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-29 13:11:25 -0500

    add a cartitem button to each product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -7,9 +7,8 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import CartItemButton from '../CartItemButton/CartItemButton';
 import Typography from '@material-ui/core/Typography';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 import Rating from '@material-ui/lab/Rating';
 
@@ -44,11 +43,7 @@ function Product(props) {
           </Box>
         </CardContent>
         <CardActions>
-          <Button variant="contained" size="small" color="primary">
-            <Typography variant="button">
-              Add to Cart <span className={classes.mobileHidden}><ShoppingCartIcon className={classes.cartIcon}/></span>
-            </Typography>
-          </Button>
+          <CartItemButton product={props.product}/>
         </CardActions>
       </Card>
     </ProductBadge>
