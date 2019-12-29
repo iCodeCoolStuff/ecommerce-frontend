1ee137ff197c5ce6b7a05174412112de10f4fefe
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-29 13:15:08 -0500

    add cart actions

---
+++ src/actions/actions.js
@@ -0,0 +1,46 @@
+import {
+  ADD_ITEM,
+  REMOVE_ITEM,
+  SET_ITEM_QUANTITY,
+  INCREMENT_ITEM_QUANTITY,
+  DECREMENT_ITEM_QUANTITY
+} from './types';
+
+let nextItemId = 0;
+
+export function addItem(product) {
+  return {
+    type: ADD_ITEM,
+    id: nextItemId++,
+    product,
+  };
+}
+
+export function removeItem(id) {
+  return {
+    type: REMOVE_ITEM,
+    id
+  };
+}
+
+export function setItemQuantity(id, quantity) {
+  return {
+    type: SET_ITEM_QUANTITY,
+    id,
+    quantity
+  };
+}
+
+export function incrementItemQuantity(id) {
+  return {
+    type: INCREMENT_ITEM_QUANTITY,
+    id
+  };
+}
+
+export function decrementItemQuantity(id) {
+  return {
+    type: DECREMENT_ITEM_QUANTITY,
+    id
+  };
+}
---
+++ src/actions/functions.js
@@ -0,0 +1,35 @@
+export function addItem(state, action) {
+  return [...state, {
+    id: action.id,
+    product: action.product,
+    quantity: 1
+  }];
+}
+
+export function removeItem(state, action) {
+  let items = state.slice();
+  let itemIndex = items.findIndex(item => item.id === action.id);
+  items.splice(itemIndex, 1);
+  return items;
+}
+
+export function setItemQuantity(state, action) {
+  let items = state.slice();
+  let itemIndex = items.findIndex(item => item.id === action.id);
+  items[itemIndex].quantity = action.quantity;
+  return items;
+}
+
+export function incrementItemQuantity(state, action) {
+  let items = state.slice();
+  let itemIndex = items.findIndex(item => item.id === action.id);
+  items[itemIndex].quantity += 1;
+  return items;
+}
+
+export function decrementItemQuantity(state, action) {
+  let items = state.slice();
+  let itemIndex = items.findIndex(item => item.id === action.id);
+  items[itemIndex].quantity -= 1;
+  return items;
+}
