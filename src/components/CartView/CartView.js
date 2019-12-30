4dfa761b8b949ed4638527e31b59cdd2c5109cd7
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-30 13:46:48 -0500

    add functionality to cartview

--- src/components/CartView/CartView.js
+++ src/components/CartView/CartView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import { Redirect } from 'react-router-dom';
 
@@ -13,83 +14,68 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 
-import { withStyles } from '@material-ui/core/styles';
-
 import styles from './CartView.styles';
 import { Typography } from '@material-ui/core';
 
-class CartView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      redirect: false
-    };
+function CartView ({items}) {
+  const classes = styles();
 
-    this.handleClick = () => {
-      this.setState({redirect: true});
-    }
+  let [redirect, setRedirect] = React.useState(false);
+
+  const handleClick = () => {
+    setRedirect(true);
   }
 
+  return(
+    <Container>
+      {redirect ? <Redirect push to="/checkout"/> : null}
+      <TableContainer className={classes.table} component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Picture</TableCell>
+              <TableCell align="left">Item</TableCell>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left">Quantity</TableCell>
+              <TableCell align="left">Total</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
 
-  render() {
-    const { classes } = this.props;
-    return(
-      <Container>
-        {this.state.redirect ? <Redirect push to="/checkout"/> : null}
-        <TableContainer className={classes.table} component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="left">Picture</TableCell>
-                <TableCell align="left">Item</TableCell>
-                <TableCell align="left">Price</TableCell>
-                <TableCell align="left">Quantity</TableCell>
-                <TableCell align="left">Total</TableCell>
+            {items.map((item, index) =>
+              <TableRow key={index}>
+                <TableCell align="left"><img style={{maxWidth: 100}} src={item.product.images.img100x100}/></TableCell>
+                <TableCell align="left">{item.product.name}</TableCell>
+                <TableCell align="left">${item.product.price.toFixed(2)}</TableCell>
+                <TableCell align="left">{item.quantity}</TableCell>
+                <TableCell align="left">${(item.product.price * item.quantity).toFixed(2)}</TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
+            )}
 
-              <TableRow>
-                <TableCell align="left"><img style={{maxWidth: 100}} src="https://placehold.it/100"/></TableCell>
-                <TableCell align="left">iRobot Roomba 670 Robot Vacuum-Wi-Fi Connectivity,</TableCell>
-                <TableCell align="left">$86</TableCell>
-                <TableCell align="left">3</TableCell>
-                <TableCell align="left">$258</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left"><img style={{maxWidth: 100}} src="https://placehold.it/100"/></TableCell>
-                <TableCell align="left">iRobot Roomba 670 Robot Vacuum-Wi-Fi Connectivity,</TableCell>
-                <TableCell align="left">$86</TableCell>
-                <TableCell align="left">3</TableCell>
-                <TableCell align="left">$258</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell align="left"><img style={{maxWidth: 100}} src="https://placehold.it/100"/></TableCell>
-                <TableCell align="left">iRobot Roomba 670 Robot Vacuum-Wi-Fi Connectivity,</TableCell>
-                <TableCell align="left">$86</TableCell>
-                <TableCell align="left">3</TableCell>
-                <TableCell align="left">$258</TableCell>
-              </TableRow>
-
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Box className={classes.totalBar} display="flex" justifyContent="flex-end">
-          <Box className={classes.mobileHidden} flexGrow={1}/>
-          <Box className={classes.totalBox}>
-            <Typography variant="h6">
-              Total: $258
-            </Typography>
-              <Button variant="contained" fullWidth={true} color="primary" onClick={()=> this.handleClick()}>
-                <Typography variant="button">
-                  Proceed to checkout
-                </Typography>
-              </Button>
-          </Box>
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box className={classes.totalBar} display="flex" justifyContent="flex-end">
+        <Box className={classes.mobileHidden} flexGrow={1}/>
+        <Box className={classes.totalBox}>
+          <Typography variant="h6">
+            Total: ${items.map(i => i.product.price * i.quantity).reduce((s, v) => s+v, 0).toFixed(2)}
+          </Typography>
+            <Button variant="contained" fullWidth={true} color="primary" onClick={() => handleClick()}>
+              <Typography variant="button">
+                Proceed to checkout
+              </Typography>
+            </Button>
         </Box>
-      </Container>
-    );
-  }
-};
+      </Box>
+    </Container>
+  );
+}
 
-export default withStyles(styles)(CartView);
+function mapStateToProps(state) {
+  return {
+    items: state.cart.items
+  };
+}
+
+export default connect(mapStateToProps)(CartView);
