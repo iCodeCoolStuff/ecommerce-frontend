8eda20ac9c8d928f34846aa809819a8019b8c38d
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-30 13:47:52 -0500

    add container to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import Container from '@material-ui/core/Container'
+
 function Footer(props) {
   return(
-    <>
+    <Container>
     <hr/>
       <div>
         &copy; {new Date().getFullYear()} All rights Reserved.
       </div>
-    </>
-  )
+    </Container>
+  );
 }
 
 export default Footer;
