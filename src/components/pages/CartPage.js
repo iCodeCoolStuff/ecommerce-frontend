5dba9376326efe1bffe53d94d966039aef41ee4f
iCodeCoolStuff <cakelord@protonmail.com>
2019-12-30 13:50:11 -0500

    render noitemcartview if there are no items in cart

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 import CartView from '../CartView/CartView';
+import NoItemCartView from '../NoItemCartView/NoItemCartView';
 
-function CartPage() {
+function CartPage({items}) {
   return(
     <>
-    <NavBar/>
-    <CartView/>
-    <Footer/>
+      <NavBar/>
+      {items.length > 0 ? <CartView/> : <NoItemCartView/>}
+      <Footer/>
     </>
   );
 }
 
-export default CartPage;
+function mapStateToProps(state) {
+  return {
+    items: state.cart.items
+  }
+}
+
+export default connect(mapStateToProps)(CartPage);
