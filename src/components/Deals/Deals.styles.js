b97d05aad51385bf50aef54c2f3f4d9c95c218da
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-03 19:24:39 -0500

    add makestyles to deal styles

--- src/components/Deals/Deals.styles.js
+++ src/components/Deals/Deals.styles.js
@@ -1,11 +1,13 @@
-const styles = theme => ({
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles(theme => ({
   title: {
     padding: 24,
     paddingTop: 32,
   },
   containerPaddingBottom: {
     paddingBottom: 24
-  }
-});
+  },
+}));
 
 export default styles;
