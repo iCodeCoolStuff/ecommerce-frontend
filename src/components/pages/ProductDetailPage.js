f7db9e7e1f6b9295a6580713f7126c626134af77
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-03 19:26:42 -0500

    add loading spinner to product detail

--- src/components/pages/ProductDetailPage.js
+++ src/components/pages/ProductDetailPage.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 
 import Container from '@material-ui/core/Container';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import NavBar from '../NavBar/NavBar';
 import ProductDetail from '../ProductDetail/ProductDetail';
@@ -10,6 +11,7 @@ import Footer from '../Footer/Footer';
 
 import Recommendations from '../Recommendations/Recommendations';
 import ProductNotFound from '../ProductNotFound';
+import ProgressDiv from '../ProgressDiv';
 
 class ProductDetailPage extends React.Component {
   constructor(props) {
@@ -25,7 +27,7 @@ class ProductDetailPage extends React.Component {
   getItem() {
     axios.get(`/v1/products/${this.props.match.params.slug}`)
     .then(res => {
-      console.log(res.data.pk);
+
       this.setState({product: res.data});
 
       axios.get(`/v1/recommendations?id=${res.data.pk}`)
@@ -63,20 +65,23 @@ class ProductDetailPage extends React.Component {
           {this.state.is404 ? <ProductNotFound/> :
 
             <div style={{paddingBottom: 32}}>
-              {this.state.loading ? <ProductDetail loading={true}/>
-              : <ProductDetail
-                loading={false}
-                img={this.state.product.images.img1920x1080}
-                name={this.state.product.name}
-                price={this.state.product.price.toFixed(2)}
-                list_price={this.state.product.list_price.toFixed(2)}
-                on_sale={this.state.product.on_sale}
-                description={this.state.product.description}
-                product={this.state.product}
-              />
-              }
-              <hr/>
-              <Recommendations items={this.state.recommendations}/>
+              {this.state.loading ? <ProgressDiv/>
+              :
+              <>
+                <ProductDetail
+                  loading={false}
+                  img={this.state.product.images.img1920x1080}
+                  name={this.state.product.name}
+                  price={this.state.product.price.toFixed(2)}
+                  list_price={this.state.product.list_price.toFixed(2)}
+                  on_sale={this.state.product.on_sale}
+                  description={this.state.product.description}
+                  product={this.state.product}
+                />
+                <hr/>
+                <Recommendations items={this.state.recommendations}/>
+              </>
+            }
             </div>
 
           }
