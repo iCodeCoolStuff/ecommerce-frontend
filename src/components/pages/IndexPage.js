8229c4b357c70913aa61fe09bb87010bbde64d97
iCodeCoolStuff <cakelord@protonmail.com>
2020-01-03 19:22:59 -0500

    add loader to index and replace carousel with a nice image

--- src/components/pages/IndexPage.js
+++ src/components/pages/IndexPage.js
@@ -1,27 +1,64 @@
 import React from 'react';
 
+import axios from 'axios'
+
 import { Container } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 import NavBar from '../NavBar/NavBar';
-import HeroCarousel from '../HeroCarousel/HeroCarousel';
+import HeroCarousel from '../HeroImage/HeroImage';
 import Deals from '../Deals/Deals';
 import Footer from '../Footer/Footer';
+import ProgressDiv from '../ProgressDiv';
 
-function IndexPage() {
-  return (
-    <>
-      <NavBar/>
-      <Container>
-        <div style={{paddingBottom: 32}}>
-          <HeroCarousel/>
-          <Deals/>
-          <Deals title="We're betting you'll love..."/>
-          <Deals title="Save on essentials"/>
-        </div>
-      </Container>
-      <Footer/>
-    </>
-  );
+const styles = theme => ({
+  wrapperDiv: {
+    marginBottom: 32
+  }
+});
+
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      items: []
+    }
+  }
+
+  getFeatured() {
+    axios.get('/v1/products/featured')
+    .then(res => {
+      this.setState({loading: false, items: res.data.slice(0, 12)})
+    })
+    .catch(err => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getFeatured();
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <>
+        <NavBar/>
+        <Container>
+          {this.state.loading ? <div className={classes.wrapperDiv}><ProgressDiv/></div> 
+            : 
+            <div style={{paddingBottom: 32}}>
+              <HeroCarousel/>
+              <Deals items={this.state.items.slice(0, 4)}/>
+              <Deals items={this.state.items.slice(4, 8)} title="We're betting you'll love..."/>
+              <Deals items={this.state.items.slice(8, 12)} title="Save on essentials"/>
+            </div>
+          }
+        </Container>
+        <Footer/>
+      </>
+    );
+  }
 }
 
-export default IndexPage;
+export default withStyles(styles)(IndexPage);
