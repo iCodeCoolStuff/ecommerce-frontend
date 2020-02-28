8eb321a42bc05fc56f799c04b841fab1fdc2f24d
iCodeCoolStuff <cakelord@protonmail.com>
2020-02-28 17:41:42 -0500

    add protected route

---
+++ src/components/ProtectedRoute.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={ props => {
+        if (props.loggedIn) {
+          return <Component {...props} />
+        } else {
+          return <Redirect to={{
+            pathname: "/login"
+          }} />
+        }
+      }}
+    />
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    loggedIn: state.auth.loggedIn
+  };
+}
+
+export default connect(mapStateToProps)(ProtectedRoute);
